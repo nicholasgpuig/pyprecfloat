fcf3eb0d46828a6c91477a614703bba9920b2382
Skyawesome73 <skyawesome73@gmail.com>
2025-04-28 23:10:01 -0500

    Add basic child node functionality and showNodes function

--- pyprecfloat/pyprecfloat.py
+++ pyprecfloat/pyprecfloat.py
@@ -8,6 +8,8 @@ import math
 # Add exponents and multiply mantissa
 # decimal_precision = 50; number of decimal places retained during initialization and math operations
 
+# Retrieve child values: recurse and get all vals, add them and divide by denominator to get decimal
+
 class PFloat:
 
     def __init__(self, num: int = 0):
@@ -25,7 +27,45 @@ class PFloat:
         shifted = num << dist_from_mpos if dist_from_mpos >= 0 else num >> (-1 * dist_from_mpos)
         mantissa = shifted & 0x7FFFFF # 11111111111111111111111; len 23
         self.pfloat = (sign << 31) | exponent << 23 | mantissa
+        self.exponent = exponent # DEBUG
+        self.child = None
+
+    # Initial n is n % d
+    # distance -1 or 1 based on direction
+    # Currently examining in space of 32 bits - if first bits are zero, shift into space and add to distance
+    def createChild(self, n, distance=0): # How to get distance ; change -1 to class constant
+        if not n: # No more bits left
+            return None
+        
+        distance -= 1 # Child values start 1 bit behind parent by default
+        
+        # Remove leading zeros and add them to distance
+        lsb = (n & -n).bit_length() - 1
+        if lsb > 0:
+            distance -= lsb
+            n >>= lsb
+
+        size = 2 # DEBUG - change to 32
+        val = n & 3 # 11
+        trailing_zeros = val.bit_length() - size
+        
+        newChild = ChildNode(val, distance)
+        newChild.child = self.createChild(n >> size, trailing_zeros)
+        return newChild
     
+    def showNodes(self):
+        print("\nPARENT")
+        print(self.toFloat())
+        print()
+        if self.child:
+            c = self.child
+            while c:
+                print("CHILD")
+                print(bin(c.value)[2:])
+                print("distance:" + str(c.distance) + "\n")
+                c = c.child
+
+
     def toInt(self) -> int:
         pos_adjustment = 23
 
@@ -43,27 +83,33 @@ class PFloat:
 
         sign = -1 if (p >> 31) & 1 else 1
 
-        exponent = ((self.pfloat & 0x7F800000) >> 23) - 127
+        exponent = ((p & 0x7F800000) >> 23) - 127
 
         mantissa_bits = p & 0x7FFFFF
         mantissa = 1 + mantissa_bits / (1 << 23)
 
         return sign * mantissa * (2 ** exponent)
 
-
-    def reverseBits(num):
+    def reverseBits(num) -> int:
         res = 0
         bitlen = len(bin(num)) - 2 #Update with pos of floating point
         for i in range(bitlen):
             a = (num >> i) & 1
             res |= a << (bitlen - 1 - i)
-            print(bin(res))
         return res
 
-    def veritfy(self):
+    def verify(self):
         return struct.unpack('f', struct.pack('I', self.pfloat))[0]
     
     def getSign(self, num: int | float):
         if num > 0:
             return num, 0
         return -1 * num, 1
+    
+
+class ChildNode:
+    
+    def __init__(self, value, distance):
+        self.value = value
+        self.distance = distance
+        self.child = None
